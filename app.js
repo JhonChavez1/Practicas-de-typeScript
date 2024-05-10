8902ca28c5017cd570821482feb933a71edd87f3
Jhon Chavez <jhonf.6520@gmail.com>
2024-05-10 16:16:44 -0500

    se gregan mas funciones

--- app.js
+++ app.js
@@ -1,10 +1,67 @@
 "use strict";
 (function () {
-    function saludar(nombre) {
-        console.table('Hola, ' + nombre); // Hola Logan
-    }
-    const wolverine = {
-        nombre: 'Logan'
+    var mensaje = 'Hola';
+    /*if ( true ) {
+        let mensaje = 'mundo';
+    }*/
+    console.log(mensaje);
+})();
+// tipos de datos
+(function () {
+    var mensaje = 'Hola';
+    var numero = 123;
+    var booleano = true; //false
+    var hoy = new Date();
+    var cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = booleano;
+    cualquiercosa = hoy;
+    var spiderman = {
+        nombre: 'Peter',
+        edad: 30
     };
-    saludar(wolverine.nombre);
-});
+})();
+// template literales = permiten construir string de una forma mas sencilla
+(function () {
+    function getEdad() {
+        return 100 + 100 + 300;
+    }
+    var nombre = 'Jhon';
+    var apellido = 'Chavez';
+    var edad = 33;
+    //const salida = nombre + apellido + edad;
+    //const salida = nombre + " "+apellido + "("+ edad +")";
+    var salida = "".concat(nombre, " ").concat(apellido, "\n        ( ").concat(edad, ")\n        ( ").concat(getEdad(), ")");
+    console.log(salida);
+})();
+// FUNCIONES: PARÁMETROS OPCIONALES O BLIGATARIOS Y POR DEFECTO
+(function () {
+    function activar(quien, // OBLIGATORIO
+    momento, //OPCIONAL: el signo de pregunta le indica a typescript que momeneto puede ser opcional
+    objeto) {
+        if (objeto === void 0) { objeto = 'batiseñal'; }
+        if (momento) {
+            console.log("".concat(quien, " activ\u00F3 la ").concat(objeto, " en la ").concat(momento));
+        }
+        else {
+            console.log("".concat(quien, " activo la ").concat(objeto, "."));
+        }
+    }
+    activar('Gordon', 'tarde');
+})();
+// FUNCIONES DE FLECHA
+(function () {
+    var miFuncion = function (a) {
+        return a.toUpperCase();
+    };
+    var miFuncionF = function (a) { return a.toUpperCase(); };
+    console.log(miFuncion('NORMAL'));
+    console.log(miFuncion('FLECHA'));
+    var sumarN = function (a, b) {
+        return a + b;
+    };
+    var sumarF = function (a, b) { return a + b; };
+    console.log(sumarN(5, 5));
+})();
+//
--- app.ts
+++ app.ts
@@ -1,14 +1,97 @@
 (function() {
 
-function saludar( nombre ) {
-    console.table( 'Hola, ' + nombre ); // Hola Logan
-}
+    const mensaje = 'Hola';
+    /*if ( true ) {
+        let mensaje = 'mundo';
+    }*/
+    console.log(mensaje);
+
+})();
 
 
-const wolverine = {
-    nombre: 'Logan'
-};
+// tipos de datos
+(function() {
+    let mensaje: string = 'Hola';
+    let numero = 123;
+    let booleano = true; //false
+    let hoy = new Date();
+
+    let cualquiercosa;
+    cualquiercosa = mensaje;
+    cualquiercosa = numero;
+    cualquiercosa = booleano;
+    cualquiercosa = hoy;
+
+    let spiderman = {
+        nombre: 'Peter',
+        edad: 30
+    };
+
+})();
 
 
-saludar( wolverine.nombre );
-})
+
+// template literales = permiten construir string de una forma mas sencilla
+(function() {
+
+    function getEdad() { 
+        return 100 + 100 +300;
+    }
+
+        const nombre     = 'Jhon';
+        const apellido   = 'Chavez'
+        const edad       = 33;
+
+        //const salida = nombre + apellido + edad;
+        //const salida = nombre + " "+apellido + "("+ edad +")";
+        const salida = `${nombre} ${apellido}
+        ( ${edad})
+        ( ${getEdad()})`;
+
+        console.log(salida);
+
+})();
+
+
+// FUNCIONES: PARÁMETROS OPCIONALES O BLIGATARIOS Y POR DEFECTO
+(function() {
+
+    function activar( quien : string,                    // OBLIGATORIO
+                    momento?: string,                    //OPCIONAL: el signo de pregunta le indica a typescript que momeneto puede ser opcional
+                    objeto  : string = 'batiseñal', ) {  // POR DEFECTO
+
+
+        if (momento){
+        console.log(`${ quien } activó la ${ objeto } en la ${momento}`);
+        } else {
+            console.log(`${ quien } activo la ${objeto}.`);
+        }
+    }
+    activar('Gordon', 'tarde');
+
+})();
+
+
+
+// FUNCIONES DE FLECHA
+( function() {
+
+    const miFuncion = function(a: string) {
+        return a.toUpperCase();
+    }
+    const miFuncionF = ( a: string ) => a.toUpperCase();
+
+    console.log(miFuncion ('NORMAL'));
+    console.log(miFuncion('FLECHA'));
+
+    const sumarN = function( a:number, b:number) {
+       return a + b;
+    }
+
+    const sumarF = ( a: number, b: number ) => a + b;
+
+    console.log( sumarN(5,5));
+
+})();
+
+//
